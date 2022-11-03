91070ff0992b2a1729e0ea1afaf13fe5580dae06
ljk4268 <ljk4268@kakao.com>
2022-11-03 16:14:16 +0900

    인스타그램만들어보기

---
+++ vuestagram/src/components/ContainerBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Post 
+      :instaDatas="instaDatas[i]" 
+      v-for="instaData,i in instaDatas" :key="instaData"/>
+    <!-- <Post/>
+    <Post/> -->
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+  name: 'containerBox',
+  components: {
+    Post,
+  },
+  props: {
+    instaDatas: Array
+  }
+}
+</script>
+
+<style>
+
+</style>
