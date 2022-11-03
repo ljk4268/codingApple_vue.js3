b6af210a1d7576307f453f00243c59697e113a61
ljk4268 <ljk4268@kakao.com>
2022-11-03 10:21:13 +0900

    데이터바인딩 props 복습

---
+++ vuelog/src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h5>{{blogDatas[0].title}}</h5>
+    <p>{{blogDatas[0].date}}</p>
+  </div>
+  <div>
+    <h5>{{blogDatas[1].title}}</h5>
+    <p>{{blogDatas[1].date}}</p>
+  </div>
+  <div>
+    <h5>{{blogDatas[2].title}}</h5>
+    <p>{{blogDatas[2].date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'List',
+  props :{
+    blogDatas : Array,
+  },
+  methods(){
+
+  }
+}
+</script>
+
+<style>
+
+</style>
